1339c2e13ffddc6ef8c01d80d2ef8d36b6f721c1
DUVAN0829 <duvthan067@gmail.com>
2026-01-06 19:38:53 -0500

    Primer commit :D

---
+++ src/main/java/co/duvan/lambda/app/SpiringbootLambdaApplication.java
@@ -0,0 +1,59 @@
+package co.duvan.lambda.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+@SpringBootApplication
+public class SpiringbootLambdaApplication {
+
+    @Bean
+    public Supplier<String> gretting() { //* GET
+        return () -> "Hello word";
+    }
+
+    @Bean
+    public Consumer<String> printParam() { //* POST
+        return System.out::println;
+    }
+
+    @Bean
+    public Function<String, String> receiveParam() { //* POST
+        return String::toUpperCase;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiringbootLambdaApplication.class, args);
+    }
+
+    //* Generar un JSON
+    @Bean
+    public Supplier<Map<String, Object>> generatedCharacter() {
+
+        return () -> {
+            Map<String, Object> character = new HashMap<>();
+            character.put("name", "Superman");
+            character.put("healthPoints", 100);
+            character.put("isHuman", true);
+            return character;
+        };
+
+    }
+
+    //* Receive a JSON and return String
+    @Bean
+    public Function<Map<String, Object>, String> receiveCharacter() {
+        return (param) -> {
+            param.forEach((key, value) -> System.out.println(key + " - " + value.toString()));
+            return "Personaje receive";
+        };
+    }
+
+}
